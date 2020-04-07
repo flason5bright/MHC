3af16fc1d0c7167a44201e3af5e3e8ba4ae3a7db
flason5bright <flason5bright@163.com>
2020-04-07 20:35:45 +0800

    Untrack some file

---
+++ MHC/MHC.DataContext/Migrations/20200407122431_ModifyUser.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MHC.DataContext.Migrations
+{
+    public partial class ModifyUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Users");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Birthday",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "StudentNum",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Birthday",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "StudentNum",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
--- MHC/MHC.DataModel/User.cs
+++ MHC/MHC.DataModel/User.cs
@@ -15,8 +15,10 @@ namespace MHC.DataModel
 
         public int Id  { get; set; }
         public string Name { get; set; }
-        public int Age { get; set; }
+        public DateTime Birthday { get; set; }
         public Gender Gender { get; set; }
+        public string Email { get; set; }
+        public string StudentNum { get; set; }
 
         public Role Role { get; set; }
 
